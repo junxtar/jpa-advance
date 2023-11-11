183c458e70b5cddb664cfac7d631af3ecafd763a
junxtar <aa1870088@naver.com>
2023-11-11 15:54:48 +0900

    @OneToMany 단방향 테스트

--- src/test/java/com/sparta/jpaadvance/relation/ManyToOneTest.java
+++ src/test/java/com/sparta/jpaadvance/relation/ManyToOneTest.java
@@ -31,17 +31,18 @@ public class ManyToOneTest {
         Food food = new Food();
         food.setName("후라이드 치킨");
         food.setPrice(15000);
-        food.setUser(user); // 외래 키(연관 관계) 설정
+//        food.setUser(user); // 외래 키(연관 관계) 설정
 
         Food food2 = new Food();
         food2.setName("양념 치킨");
         food2.setPrice(20000);
-        food2.setUser(user); // 외래 키(연관 관계) 설정
+//        food2.setUser(user); // 외래 키(연관 관계) 설정
 
         userRepository.save(user);
         foodRepository.save(food);
         foodRepository.save(food2);
     }
+
     @Test
     @Rollback(value = false)
     @DisplayName("N대1 양방향 테스트 : 외래 키 저장 실패")
@@ -58,8 +59,8 @@ public class ManyToOneTest {
         // 외래 키의 주인이 아닌 User 에서 Food 를 저장해보겠습니다.
         User user = new User();
         user.setName("Robbie");
-        user.getFoodList().add(food);
-        user.getFoodList().add(food2);
+//        user.getFoodList().add(food);
+//        user.getFoodList().add(food2);
 
         userRepository.save(user);
         foodRepository.save(food);
@@ -85,8 +86,8 @@ public class ManyToOneTest {
         // 해당 메서드에 외래 키(연관 관계) 설정 food.setUser(this); 추가
         User user = new User();
         user.setName("Robbie");
-        user.addFoodList(food);
-        user.addFoodList(food2);
+//        user.addFoodList(food);
+//        user.addFoodList(food2);
 
         userRepository.save(user);
         foodRepository.save(food);
@@ -103,12 +104,12 @@ public class ManyToOneTest {
         Food food = new Food();
         food.setName("고구마 피자");
         food.setPrice(30000);
-        food.setUser(user); // 외래 키(연관 관계) 설정
+//        food.setUser(user); // 외래 키(연관 관계) 설정
 
         Food food2 = new Food();
         food2.setName("아보카도 피자");
         food2.setPrice(50000);
-        food2.setUser(user); // 외래 키(연관 관계) 설정
+//        food2.setUser(user); // 외래 키(연관 관계) 설정
 
         userRepository.save(user);
         foodRepository.save(food);
@@ -123,7 +124,7 @@ public class ManyToOneTest {
         System.out.println("food.getName() = " + food.getName());
 
         // 음식을 주문한 고객 정보 조회
-        System.out.println("food.getUser().getName() = " + food.getUser().getName());
+//        System.out.println("food.getUser().getName() = " + food.getUser().getName());
     }
 
     @Test
@@ -134,10 +135,10 @@ public class ManyToOneTest {
         System.out.println("user.getName() = " + user.getName());
 
         // 해당 고객이 주문한 음식 정보 조회
-        List<Food> foodList = user.getFoodList();
-        for (Food food : foodList) {
-            System.out.println("food.getName() = " + food.getName());
-            System.out.println("food.getPrice() = " + food.getPrice());
-        }
+//        List<Food> foodList = user.getFoodList();
+//        for (Food food : foodList) {
+//            System.out.println("food.getName() = " + food.getName());
+//            System.out.println("food.getPrice() = " + food.getPrice());
+//        }
     }
 }
